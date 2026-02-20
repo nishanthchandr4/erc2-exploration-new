e5baf0f21178314ea47d8526f9f1215a0fc44063
naporamaksym-crypto <naporamaksym@gmail.com>
2026-02-20 10:50:24 -0500

    modified:   lib 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,20 @@
 #include <FEHServo.h>
 #include <Arduino.h>
 
-
+FEHMotor right_motor(FEHMotor::Motor0,9.0);
+FEHMotor left_motor(FEHMotor::Motor2,9.0);
 AnalogInputPin sensor(FEHIO::Pin2);
+FEHServo servo(FEHServo::Servo0);
 
 
 void ERCMain()
 {
-    // Your code here!
-    sensor.Value();
+    
 
-    // Or just use the TestGUI function
-    TestGUI();
+    FEHServo servo(FEHServo::Servo0);
+    servo.TouchCalibrate();
+    //while(sensor.Value()>1 && sensor.Value()<6){
 
+    
+    
 }
